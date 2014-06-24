f7bb6141b559d222300efc29f51f8176b4201d6a
Emile 'iMil' Heitor <imil@NetBSD.org>
2014-06-24 13:28:31 +0200

    better tooltips, ^C handling

--- upt.py
+++ upt.py
@@ -47,16 +47,26 @@ class Chkupdate:
 
         self.update_running = False
 
-        signal.signal(signal.SIGINT, self._signal_handler)
+        self.notifier = None
+        self.watchdir = '/var/lib/apt'
 
-    def _signal_handler(self, signal, frame):
+        self.tt_state = {
+            'upgrade': 'Upgrades available',
+            'essential': 'Urgent upgrades available',
+            'noop': 'No upgrade available',
+            'working': 'Operations in progress'
+        }
+
+    def quit(self):
+        logger.info('stopping notifier thread')
+        self.notifier.stop()
         logger.info('exiting')
-        sys.exit(1)
+        sys.exit(0)
 
     def set_state(self, state):
         icon = eval('{0}_icon'.format(state))
         self.icon.set_from_file(icon)
-        self.icon.set_tooltip(state)
+        self.icon.set_tooltip(self.tt_state[state])
 
     def aptchk(self):
         apt_cache = apt.Cache() #High level
@@ -83,7 +93,7 @@ class Chkupdate:
         logger.info('entering update')
         if self.update_running is False:
             self.update_running = True
-            self.icon.set_from_file(working_icon)
+            self.set_state('working')
             lst = self.aptchk()
             if lst:  # packages to upgrade
                 state = 'upgrade'
@@ -107,9 +117,9 @@ class Chkupdate:
     def main(self):
         wm = pyinotify.WatchManager()
         mask = pyinotify.IN_DELETE | pyinotify.IN_CREATE | pyinotify.IN_MODIFY
-        notifier = pyinotify.ThreadedNotifier(wm, self.update)
-        wm.add_watch('/var/lib/apt', mask)
-        notifier.start()
+        self.notifier = pyinotify.ThreadedNotifier(wm, self.update)
+        wm.add_watch(self.watchdir, mask)
+        self.notifier.start()
 
         t = Thread(target=self.timer_update)
         t.daemon = True
@@ -120,4 +130,7 @@ class Chkupdate:
 if __name__ == "__main__":
     app = Chkupdate()
     logger.info('starting upt')
-    app.main()
+    try:
+        app.main()
+    except KeyboardInterrupt:
+        app.quit()
